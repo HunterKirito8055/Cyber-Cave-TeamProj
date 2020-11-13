be71541fac3ad7ea09bb56a9767adab04c19ba2e
Vishal Kumar Varma <sirikondavishal8055@gmail.com>
2020-11-13 12:07:22 +0530

    Bullet Deflection and detection DONE
    addded a bullet deflection script and made to detect the other GAMEOBJECTS's TAG name "DEFLECTION". so use this tag name to any game object in the game.

---
+++ Assets/Scripts/Player_Scripts/BulletDeflect.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDeflect : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Vector3 lastvel;
+    CircleCollider2D cc2d;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        cc2d = GetComponent<CircleCollider2D>();
+    }
+    private void Start()
+    {
+        if(cc2d.isTrigger.Equals(false))
+        {
+            //cc2d.isTrigger = true;
+        }
+    }
+    private void Update()
+    {
+        lastvel = rb.velocity;
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Deflection")
+        {
+            Debug.Log(collision.gameObject.tag);
+            cc2d.isTrigger = false;
+            var speed = lastvel.magnitude;
+            var direction = Vector3.Reflect(lastvel.normalized, collision.contacts[0].normal);
+            rb.velocity = direction * Mathf.Max(speed, 0f);
+            
+        }
+        else
+        {
+            cc2d.isTrigger = true;
+        }
+    }
+}
