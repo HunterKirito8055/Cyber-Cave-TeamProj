f4034ecdfe88dca96a6cc82ad4782dc666f70e27
Stephenjacob2000 <36294217+Stephenjacob2000@users.noreply.github.com>
2021-01-03 18:17:35 +0530

    scene: HealthIntegration

--- Assets/Scripts/Enemy_Scripts/eHealth_Melee.cs
+++ Assets/Scripts/Enemy_Scripts/eHealth_Melee.cs
@@ -2,32 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class eHealth_LongRanged : MonoBehaviour
+public class eHealth_Melee : MonoBehaviour
 {
     public float eHealthCounter = 1.0f;
     public bool recieveDamage = true;
     public Animator anim;
 
-    void Start()
+    public void Start()
     {
-        anim = GetComponent<Animator>();
+       anim = GetComponent<Animator>();
     }
 
-
     public IEnumerator OnTriggerEnter2D(Collider2D collision)
-    {        
+    {
+       
+
         if (collision.gameObject.tag == "playerObj")
         {
             if (eHealthCounter > 0 && recieveDamage == true)
             {
-                anim.SetTrigger("Impact");
-                eHealthCounter -= 0.20f;
+                //anim.SetTrigger("Impact");
+                eHealthCounter -= 0.40f;
             }
             else if (eHealthCounter <= 0 && recieveDamage == true)
             {
                 recieveDamage = false;
-                gameObject.GetComponent<EnemyRanged>().enabled = false;
-                anim.SetTrigger("Die");
+                gameObject.GetComponent<EnemyMotor>().enabled = false;
+                //anim.SetTrigger("Die");
                 yield return new WaitForSeconds(2f);
                 Destroy(gameObject);
             }
