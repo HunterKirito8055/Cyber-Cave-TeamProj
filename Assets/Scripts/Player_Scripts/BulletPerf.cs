d474ea86bfc7840a787d7e8e650510aa3b588fc8
Vishal Kumar Varma <sirikondavishal8055@gmail.com>
2021-01-03 21:15:17 +0530

    Revert "Revert "longrane set""
    This reverts commit 6ff80f373bbeea29f1379728cf2f72e10d6c5398.

---
+++ Assets/Scripts/Player_Scripts/BulletPerf.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPerf : MonoBehaviour
+{
+    public float speed = 10f;
+    public Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * speed;
+        StartCoroutine(DisableFire(2f));
+    }
+
+    IEnumerator DisableFire(float t)
+    {
+        yield return new WaitForSeconds(t);
+        gameObject.SetActive(false);
+        Destroy();
+    }
+    void Destroy()
+    {
+        Destroy(gameObject);
+    }
+
+    
+}
